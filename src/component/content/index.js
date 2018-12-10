e5972e0eb9b0452191e9ccf66473f72aa83f7237
TimeAway <37588387+TimeAway@users.noreply.github.com>
2018-12-10 10:32:39 +0800

    采用新版的Refs使用方式，旧版String 类型的 Refs已被抛弃

--- src/component/content/index.js
+++ src/component/content/index.js
@@ -13,31 +13,32 @@ class Content extends Component {
 			currMusic: 0	// 当前播放的音乐
 		}
 
+		this.player = React.createRef();
 		this.choseMusic = this.choseMusic.bind(this);
 	}
 
 	componentWillMount(){
-		const $this = this;
+		const that = this;
 		$.ajax({
 			type: 'get',
 			dataType: 'json',
 			async: false,	// 这里使用同步的方式
 			url: '/mock/music.json',
 			success(data){
-				$this.setState(data)
+				that.setState(data)
 			}
 		})
 	}
 
 	choseMusic(id){
-		this.refs.player.setCurrentMusic(id);
+		this.player.current.setCurrentMusic(id);
 	}
 
 	render(){
 		return (
 			<div className="content">
 				<div className="content-left">
-					<Player list={this.state.musicList} ref="player" />
+					<Player list={this.state.musicList} ref={this.player} />
 				</div>
 				<div className="content-right">
 					<List list={this.state.musicList} choseMusic={this.choseMusic} />
